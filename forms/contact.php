1d82dd0e802c0bdc101e76e88abf44040bb8502f
Flinch <Abdulmalikibrahim072@gmail.com>
2021-03-26 15:24:07 -0400

    email form check

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+ $contact->smtp = array(
+    'host' => 'smtp-relay.sendinblue.com',
+    'username' => 'lorembaker@gmail.com',
+    'password' => '9H0hzyFW5U8wOTbK',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
